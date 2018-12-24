6371e7baf0f4e20a07443a5f5ee9385979d036e5
pravdin97 <pravdin.ev97@gmail.com>
2018-12-24 22:37:12 +0700

    3rd lab completed

--- src/Pack/InformExpert.java
+++ src/Pack/InformExpert.java
@@ -1,15 +1,32 @@
 package Pack;
 
 import Delivery.Delivery;
+import Pack.Composite.CompositePackage;
+import Pack.Composite.Order;
+import Pack.Composite.Package;
+import Pack.Iterator.Iterator;
 
 public class InformExpert {
     private Delivery company;
+    public int countOrders;
 
     public InformExpert(Delivery company) {
         this.company = company;
     }
 
     public void count() {
-
+        for (Package p :company.getPackages()) {
+            if (p.getClass() == Order.class)
+                countOrders++;
+            else {
+                CompositePackage cp = (CompositePackage)p;
+                Iterator i = cp.createIterator();
+                while(i.hasNext())
+                {
+                    countOrders++;
+                    i.next();
+                }
+            }
+        }
     }
 }
